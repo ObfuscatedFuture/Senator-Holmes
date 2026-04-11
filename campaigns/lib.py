48eb24b8972d5295dd75f1b45c86923f4db7828b
Varun Singh <VSpoet49@gmail.com>
2026-04-11 19:11:39 -0400

    feat: get promises that are on different pages, put in class

--- campaigns/lib.py
+++ campaigns/lib.py
@@ -1,37 +1,37 @@
-import re
-
+import requests
 from bs4 import BeautifulSoup
-from urllib.request import Request, urlopen, urlcleanup
-from requests import get
+
 
 def find_promises_page(homeurl):
-    request = Request(homeurl)
-    request.add_header("User-Agent",
-                       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.6 Safari/605.1.15")
     return f"{homeurl}/issues"
     #
     # with urlopen(request) as response:
     #     soup = BeautifulSoup(response, "html.parser")
     #     promise_heading = soup.body.find("h1", string=re.compile("Day One Promises"))
 
-def scrape_promise_single_page(url):
-    """
-    Scrapes a page with a single promise
-    :param url:
-    :return:
-    """
-    soup = construct_request(url)
-    return map(lambda x: x.get_text(), soup.body.select("header + * p"))
+
+class CampaignScraper:
+
+    def __init__(self):
+        self.session = requests.Session()
+        self.session.headers.update({'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.6 Safari/605.1.15'})
+
+    def construct_soup(self, url):
+        return BeautifulSoup(self.session.get(url).content, "html.parser")
+
+    def scrape_promise_single_page(self, url):
+        """
+        Scrapes a page with a single promise
+        :param url:
+        :return:
+        """
+        soup = self.construct_soup(url)
+        return "\n".join(map(lambda x: x.get_text(), soup.body.select("header + * p")))
 
 
-def scrape_promises_single_page(url):
-    request = Request(url)
-    request.add_header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.6 Safari/605.1.15")
-
-    promises = []
-
-    with urlopen(request) as response:
-        soup = BeautifulSoup(response, "html.parser")
+    def scrape_promises_single_page(self, url):
+        soup = self.construct_soup(url)
+        promises = []
 
         promises_html = soup.body.select("ol")
         main_html = soup.body.select_one("header").find_next_sibling()
@@ -40,29 +40,20 @@ def scrape_promises_single_page(url):
         for promise in main_html.find_all("p"):
             promises.append(promise.get_text())
 
-    return promises
+        return promises
 
-def scrape_promises_linked_pages(overview):
-    """
-    Scrapes campaign websites where each campaign promise is on a separate page, all linked from a page listing out titles
-    for those promises
-    :param overview: URL of the issues/campaign promises overview
-    :return:Complete list of strings each representing a campaign promise from the website
-    """
-    promises = []
-    soup = construct_request(overview)
+    def scrape_promises_linked_pages(self, overview):
+        """
+        Scrapes campaign websites where each campaign promise is on a separate page, all linked from a page listing out titles
+        for those promises
+        :param overview: URL of the issues/campaign promises overview
+        :return:Complete list of strings each representing a campaign promise from the website
+        """
+        promises = []
+        soup = self.construct_soup(overview)
 
-    promises_html = soup.body.select("header ~ * a")
-    for link in promises_html:
-        promises.append(scrape_promise_single_page(link['href']))
+        promises_html = soup.body.select("header + * a")
+        for link in promises_html:
+            promises.append(self.scrape_promise_single_page(link['href']))
 
-    return promises
-
-
-def construct_request(url):
-    request = Request(url)
-    request.add_header("User-Agent",
-                       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.6 Safari/605.1.15")
-    response = urlopen(request)
-    soup = BeautifulSoup(response, "html.parser")
-    return soup
+        return promises
--- campaigns/main.py
+++ campaigns/main.py
@@ -1,16 +1,19 @@
-import lib
-#
-davemccormick = lib.scrape_promises_single_page("https://www.davemccormickpa.com/day-one-promises/")
+from lib import CampaignScraper, find_promises_page
+
+cs = CampaignScraper()
+
+davemccormick = cs.scrape_promises_single_page("https://www.davemccormickpa.com/day-one-promises/")
 print("*****Dave McCormick's Promises******")
 for i in davemccormick: print(i)
 print("\n\n\n")
 # angelaalsobrooks = scrape_promises_single_page("https://www.angelaalsobrooks.com/priorities")
 # for i in angelaalsobrooks: print(i)
 print("*****Jim Bank's Promises******")
-jimbanks = lib.scrape_promises_single_page("https://banksforsenate.com/issues/")
+jimbanks = cs.scrape_promises_single_page("https://banksforsenate.com/issues/")
 for i in jimbanks: print(i)
 print("\n\n\n")
 
 print("****Jim Justice's Promises*****")
-jimjusticepage = lib.find_promises_page("https://jimjusticewv.com")
-jimjustice = lib.scrape_promises_single_page(jimjusticepage)
+jimjusticepage = find_promises_page("https://jimjusticewv.com")
+jimjustice = cs.scrape_promises_linked_pages(jimjusticepage)
+for x in jimjustice: print(x)
