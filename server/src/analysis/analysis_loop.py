e46e3b8023d583a84747284d22281f28f525d059
Chase LaBarre <86638507+ObfuscatedFuture@users.noreply.github.com>
2026-04-11 14:40:57 -0400

    feat: Update llm analysis_loop.py

--- server/src/analysis/analysis_loop.py
+++ server/src/analysis/analysis_loop.py
@@ -1,3 +1,15 @@
 ## This should do the full llm.py loop with all bills + the validation step
 import llm_validation
+import llm
+
+bill_text_sample = ""
+
+# Initialize the BillAnalyzer
+BillAnalyzer = llm.BillAnalyzer()
+
+
+json_from_llm = BillAnalyzer.llm_analysis(bill_text_sample)
+
+validated_json = llm_validation.BillClassification.model_validate_json(json_from_llm)
+
 
