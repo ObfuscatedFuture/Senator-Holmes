7cb4f5268946d6fd60d05a4193f5916c76e2f9c7
sid tanjore <stanjore@terpmail.umd.edu>
2026-04-11 14:58:47 -0400

    feat: minor changes to analysis

--- server/src/analysis/analysis_loop.py
+++ server/src/analysis/analysis_loop.py
@@ -1,12 +1,14 @@
 ## This should do the full llm.py loop with all bills + the validation step
 import llm_validation
 import llm
+import json
 
-bill_text_sample = ""
-
+f = open('test_bill.txt')
+bill_text_sample = f.read()
 # Initialize the BillAnalyzer
-BillAnalyzer = llm.BillAnalyzer()
 
+
+BillAnalyzer = llm.BillAnalyzer()
 
 json_from_llm = BillAnalyzer.llm_analysis(bill_text_sample)
 
@@ -16,8 +18,8 @@ validated_json = llm_validation.BillClassification.model_validate_json(json_from
 
 
 # turns plaintext to json, then json to array 
-        data = json.loads(completion.choices[0].message.content)
-        categories = [
-            [k, v["score"]] 
-            for k, v in data["categories"].items() if v["score"] != 0
+data = json.loads(validated_json)
+categories = [
+    [k, v["score"]] 
+    for k, v in data["categories"].items() if v["score"] != 0
         ]
