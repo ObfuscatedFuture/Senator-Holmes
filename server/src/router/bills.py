e8b06bd94f57a4ddc2997adc9113e64b9d28d4a3
kevinli3633 <kevinli3633@gmail.com>
2026-04-11 17:43:54 -0400

    feat: changed return type of get bill to return whole bill

--- server/src/router/bills.py
+++ server/src/router/bills.py
@@ -4,7 +4,7 @@ from src.database.model import Bill
 def create_bill(bill: Bill) -> int:
     bill_data = bill.model_dump(by_alias=True, exclude_none=True)
     bills_collection = get_collection("Bill")
-    if (get_bill_score(bill.bill_id) != []):
+    if (get_bill(bill.bill_id) is not None):
         print(f"Bill with ID {bill.bill_id} already exists.")
         return -1
     try: 
@@ -14,14 +14,14 @@ def create_bill(bill: Bill) -> int:
         return -1
     return 0
 
-def get_bill_score(bill_id: int) -> list:
+def get_bill(bill_id: int) -> Bill:
     bill_collection = get_collection("Bill")
     try:
         bill = bill_collection.find_one({"bill_id": bill_id})
         if bill is None:
             print(f"No bill found with bill_id: {bill_id}")
-            return []
-        return bill.get("category_scores", [])
+            return None
+        return bill
     except Exception as e:
-        print(f"Error occurred while retrieving bill score: {e}")
-        return []
+        print(f"Error occurred while retrieving bill: {e}")
+        return None
