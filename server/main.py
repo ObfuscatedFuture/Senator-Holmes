f56d15e6d0fa74e97ec25932ffbffb3c2b25b376
kevinli3633 <kevinli3633@gmail.com>
2026-04-11 18:31:27 -0400

    fix: frontend path

--- server/main.py
+++ server/main.py
@@ -4,4 +4,4 @@ from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 
 # "html=True" automatically serves index.html at the root of the mount path
-app.mount("/", StaticFiles(directory="../client", html=True), name="static")
+app.mount("/", StaticFiles(directory="./client", html=True), name="static")
